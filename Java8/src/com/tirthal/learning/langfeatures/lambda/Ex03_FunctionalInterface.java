c175372e9dee1819684c62f31ad0a579a1b6505e
Tirthal Patel <patel.tirthal@gmail.com>
2017-01-08 14:42:25 +0530

    Java 8: minor updates

--- Java8/src/com/tirthal/learning/langfeatures/lambda/Ex03_FunctionalInterface.java
+++ Java8/src/com/tirthal/learning/langfeatures/lambda/Ex03_FunctionalInterface.java
@@ -1,22 +1,25 @@
 package com.tirthal.learning.langfeatures.lambda;
 
 /**
- * This is functional interface (also referred as, single method interface = an interface with only one abstract method)
+ * This is functional interface (also referred as, single method interface = an interface with only one abstract method
+ * + may have default methods, static methods, methods from the Object class)
  * 
- * <p>A functional interface can be annotated using - @FunctionalInterface (Optional). It is just here for convenience, the
+ * <p>
+ * A functional interface can be annotated using - @FunctionalInterface (Optional). It is just here for convenience, the
  * compiler can tell us whether the interface is functional or not!
  * 
  * @author tirthalp
  */
 @FunctionalInterface
-interface Ex03_HelloWorld {
-	void sayHello(String name);
+interface Ex03_HelloWorld<T> {
+	
+	void sayHello(T t);
 
 	/*
 	 * If you have more than one method, then @FunctionalInterface would inform compiler to throw compile time error -
 	 * "Invalid '@FunctionalInterface' annotation; Ex03_HelloWorld is not a functional interface"
 	 */
-	// void sayBye(String name); // Uncomment to see compile time error
+	// void sayBye(T t); // Uncomment to see compile time error
 }
 
 /**
@@ -32,8 +35,11 @@ public class Ex03_FunctionalInterface {
 		 * Ex03_HelloWorld interface, then it would not be functional interface anymore. And you would see this compile
 		 * time error - "The target type of this expression must be a functional interface"
 		 */
-		Ex03_HelloWorld hw = (String name) -> System.out.println("Hello " + name);
-		hw.sayHello("Tirthal");
+		Ex03_HelloWorld<String> hw1 = s -> System.out.println("Hello " + s);
+		hw1.sayHello("Tirthal");
+		
+		Ex03_HelloWorld<Integer> hw2 = n -> System.out.println("Hello World, " + n + " times");
+		hw2.sayHello(10);
 	}
 
 }
