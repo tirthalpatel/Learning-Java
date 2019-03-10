64f9c0b30a9a69b72130e8606a1beead15fdf828
Tirthal Patel <patel.tirthal@gmail.com>
2019-03-10 23:06:57 +0530

    Java11: added initial copy without sample code

---
+++ Java11/src/main/java/com/tirthal/learning/libfeatures/NewHttpClientApiDemo.java
@@ -0,0 +1,8 @@
+package com.tirthal.learning.libfeatures;
+
+public class NewHttpClientApiDemo {
+
+    public static void main(String[] args) {
+        // Example code to be added...
+    }
+}
---
+++ Java11/src/main/java/com/tirthal/learning/libfeatures/NewMethodsInStringDemo.java
@@ -0,0 +1,8 @@
+package com.tirthal.learning.libfeatures;
+
+public class NewMethodsInStringDemo {
+
+    public static void main(String[] args) {
+        // Example code to be added...
+    }
+}
